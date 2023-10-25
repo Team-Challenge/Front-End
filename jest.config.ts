9bc3b3b4c08944bc73598f17cff1b31ff8208c5a
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-10-25 12:16:49 +0300

    feat: add config for testing

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  globals: {
+    tsconfig: './tsconfig.json',
+    babelConfig: true,
+  },
+};
