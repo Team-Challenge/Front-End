d16683a6a0c54ce9db163b1c126309794ae6caf6
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-11-21 22:29:39 +0200

    fix: fix modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,23 +1,23 @@
+import { useAppDispatch } from '@/hooks/reduxHook';
+import { closeModal } from '@/store/modalSlice';
 import { ModalProps } from '@/types';
+import { CloseIcon } from '../icons/CloseIcon';
 import s from './Modal.module.scss';
 
-export const Modal = ({
-  isOpen,
-  onClose,
-  children,
-  buttonText,
-}: ModalProps) => {
-  if (!isOpen) return null;
+export const Modal = ({ children }: ModalProps) => {
+  const dispatch = useAppDispatch();
+
+  const handleCloseModal = () => {
+    dispatch(closeModal());
+  };
 
   return (
     <div className={s.modal}>
-      <div className={s.modal_block}>
-        {children}
-        {buttonText && (
-          <button type='submit' className={s.modal_btn} onClick={onClose}>
-            {buttonText}
-          </button>
-        )}
+      <div className={s.modal_wrap}>
+        <div className={s.modal_block}>{children}</div>
+        <button className={s.modal_close} onClick={handleCloseModal}>
+          <CloseIcon />
+        </button>
       </div>
     </div>
   );
