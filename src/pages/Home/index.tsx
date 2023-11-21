3814a351df4d661354ccf5b58427296ddc18c92d
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-11-21 22:16:44 +0200

    feat: add a demo of the Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import s from './Home.module.scss';
+
+export const Home = () => {
+  return(
+    <main className={s.home}>
+      <h3>
+        Welcome to our marketplace.
+      </h3>
+    </main>
+  )
+}
