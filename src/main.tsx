2df0a5b447db1579d49fee30b4b5d94c3921923c
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-11-21 22:05:02 +0200

    fix: fix import for global styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 import { App } from './App';
-import './styles/index.scss';
+import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
