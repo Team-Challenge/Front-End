fd712596ca3997fcf057cfb0241e6341d055ef27
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-02 20:08:46 +0200

    fix: update pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
-import { UserPanel } from './UserPanel';
-import { PageNotFound } from './PageNotFound';
 import { Home } from './Home';
+import { PageNotFound } from './PageNotFound';
+import { Profile } from './userPanel/Profile';
+import { Purchases } from './userPanel/Purchases';
+import { FavoriteProducts } from './userPanel/FavoriteProducts';
+import { Messages } from './userPanel/Messages';
+import { Store } from './userPanel/Store';
+import { Settings } from './userPanel/Settings';
+import { Products } from './storePanel/Products';
+import { StoreManagement } from './storePanel/StoreManagement';
+import { Orders } from './storePanel/Orders';
+import { Reviews } from './storePanel/Reviews';
 
-export { Home, UserPanel, PageNotFound };
+export {
+  Home,
+  PageNotFound,
+  Profile,
+  Purchases,
+  FavoriteProducts,
+  Messages,
+  Store,
+  Settings,
+  Products,
+  StoreManagement,
+  Orders,
+  Reviews
+};
