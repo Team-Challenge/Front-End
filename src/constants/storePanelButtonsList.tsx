6e530122269a3ad152b1c7b3d0a521e8c4b7b992
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-02 20:08:37 +0200

    feat: add buttons list for store panel

---
+++ src/constants/storePanelButtonsList.tsx
@@ -0,0 +1,34 @@
+import { Icon } from '@iconify/react';
+
+export const storePanelButtonsList = [
+  {
+    id: 1,
+    title: 'Мої товари',
+    pathToPage: 'products',
+    icon: <Icon icon='solar:layers-minimalistic-outline' />,
+  },
+  {
+    id: 2,
+    title: 'Керування магазином',
+    pathToPage: 'settings',
+    icon: <Icon icon='solar:tuning-2-outline' />,
+  },
+  {
+    id: 3,
+    title: 'Замовлення',
+    pathToPage: 'orders',
+    icon: <Icon icon='solar:box-outline' />,
+  },
+  {
+    id: 4,
+    title: 'Повідомлення',
+    pathToPage: 'messages',
+    icon: <Icon icon='solar:letter-linear' />,
+  },
+  {
+    id: 5,
+    title: 'Відгуки та рейтинг',
+    pathToPage: 'reviews',
+    icon: <Icon icon='solar:chat-round-outline' />,
+  }
+];
