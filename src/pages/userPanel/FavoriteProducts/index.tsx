17c99bb38f074cb723c45ae43b472e98a85565c6
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-04 17:00:19 +0200

    feat: add files for user panel

---
+++ src/pages/userPanel/FavoriteProducts/index.tsx
@@ -0,0 +1,9 @@
+import s from './FavoriteProducts.module.scss';
+
+export const FavoriteProducts = () => {
+  return (
+    <section>
+      <h4>Обрані товари</h4>
+    </section>
+  );
+};
---
+++ src/pages/userPanel/Settings/UserPassword.tsx
@@ -0,0 +1,41 @@
+import { useFormContext } from 'react-hook-form';
+import { PasswordInput } from '@/components/UI/PasswordInput';
+import s from './Settings.module.scss';
+
+export const UserPassword = () => {
+  const methods = useFormContext();
+  const { getValues, watch } = methods;
+
+  const oldPassword = watch('current_password');
+  const newPassword = watch('new_password');
+  const newPasswordRepeat = watch('new_password_repeat');
+
+  const isAnyPasswordFilled = Boolean(
+    newPassword || newPasswordRepeat || oldPassword,
+  );
+
+  return (
+    <label className={s.form_label}>
+      Налаштування паролю
+      <PasswordInput
+        id='current_password'
+        placeholder='Старий пароль'
+        required={isAnyPasswordFilled}
+      />
+      <PasswordInput
+        id='new_password'
+        placeholder='Новий пароль'
+        required={isAnyPasswordFilled}
+      />
+      <PasswordInput
+        id='new_password_repeat'
+        placeholder='Повторіть пароль'
+        required={isAnyPasswordFilled}
+        validate={(value: string) =>
+          value === getValues('new_password') || 'Введені паролі не співпадають'
+        }
+        isRepeatPassword
+      />
+    </label>
+  );
+};
