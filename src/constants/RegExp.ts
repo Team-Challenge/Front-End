f225c6582c4deb8ec7e9a868096c9412c2b18d1a
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-04 14:31:35 +0200

    fix: update regex for full name and email

--- src/constants/RegExp.ts
+++ src/constants/RegExp.ts
@@ -1,4 +1,4 @@
-export const FULL_NAME_REGEX = /^[a-zA-Z\xC0-\uFFFF]+([ \-']{0,1}[a-zA-Z\xC0-\uFFFF]+){1,4}\s[a-zA-Z\xC0-\uFFFF]+([ \-']{0,1}[a-zA-Z\xC0-\uFFFF]+){0,2}$/u;
-export const EMAIL_REGEX = /^[\p{L}\p{N}_.-]+@([\p{L}\p{N}-]+\.)+[\p{L}\p{N}-]{2,}$/gu;
+export const FULL_NAME_REGEX = /^(?!.*--)(?!.*``)(?!.*'')[A-Za-zА-Яа-яІіЇїЄє '`-]{2,30}$/u;
+export const EMAIL_REGEX = /^(?!.*--)(?!.*__)(?!.*\.\.)(?!.*-$)(?!.*\.$)(?!.*^\..*$)(?!.*\.$)(?!.*^-)[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/g;
 export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/;
 export const PHONE_NUMBER_REGEX = /^\+380\d{9}$/;
