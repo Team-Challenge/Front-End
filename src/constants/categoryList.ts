97dfceb7afe18a5de7e1fa29c868d42fbd61e592
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-11 14:38:53 +0200

    feat: add category list

---
+++ src/constants/categoryList.ts
@@ -0,0 +1,39 @@
+import head from '@/assets/category-icons/head.svg';
+import ears from '@/assets/category-icons/ears.svg';
+import neck from '@/assets/category-icons/neck.svg';
+import hands from '@/assets/category-icons/hands.svg';
+import accessories from '@/assets/category-icons/accessories.svg';
+import sets from '@/assets/category-icons/sets.svg';
+
+export const categoryList = [
+  {
+    id: 1,
+    label: 'на голову',
+    icon: head,
+  },
+  {
+    id: 2,
+    label: 'на вуха',
+    icon: ears,
+  },
+  {
+    id: 3,
+    label: 'на шию',
+    icon: neck,
+  },
+  {
+    id: 4,
+    label: 'на руки',
+    icon: hands,
+  },
+  {
+    id: 5,
+    label: 'аксесуари',
+    icon: accessories,
+  },
+  {
+    id: 6,
+    label: 'набори',
+    icon: sets,
+  }
+];
