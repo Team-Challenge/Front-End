209ff8b427917ce0cbae759f78de1aad10196b08
Andrii Moroziuk/PC <morozuk44@gmail.com>
2023-12-11 13:16:30 +0200

    menu and styles

--- src/components/UserDropdownMenu/index.tsx
+++ src/components/UserDropdownMenu/index.tsx
@@ -5,28 +5,43 @@ import s from './UserDropdownMenu.module.scss';
 import { userPanelButtonsList } from '@/constants/userPanelButtonsList';
 import { NavLink } from 'react-router-dom';
 
-export const UserDropdownMenu = ({ handleOpenModal }: UserDropdownMenuProps) => {
+export const UserDropdownMenu = ({ handleOpenModal, setDropdownOpen }: UserDropdownMenuProps) => {
   const { isAuth } = useAppSelector((state) => state.auth);
 
   return (
     <div className={s.dropdownMenu}>
       {isAuth ? (
         <div>
-          menu
+          {userPanelButtonsList.map((button) => (
+            <NavLink
+              to={`account/${button.pathToPage}`}
+              key={button.id}
+              onClick={() => setDropdownOpen(false)}
+              className={({ isActive }) =>
+              isActive
+                ? `cabinet-sidebar-nav_btn cabinet-sidebar-nav_active`
+                : 'cabinet-sidebar-nav_btn'
+            }
+              >
+                {button.icon}
+                {button.title}
+            </NavLink>
+            )
+          )}
         </div>
       ) : (
-        <>
+      <div className={s.dropdownMenu_auth}>
           <ButtonUI
             label='Увійти'
             onClick={() => handleOpenModal(true)}
           />
-          <div className={s.dropdownMenu_registration}>
+          <div className={s.dropdownMenu_auth_registration}>
             <p>Вперше тут?</p>
             <button onClick={() => handleOpenModal(false)}>
               Зареєструватися
             </button>
           </div>
-        </>
+        </div>
       )}
     </div>
   );
