4bdd0b8e11abdbb958b918b30ea3fdcf1f4c3e5a
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-13 11:40:46 +0200

    fix: change user phone number input

--- src/pages/userPanel/Settings/UserPhoneNumber.tsx
+++ src/pages/userPanel/Settings/UserPhoneNumber.tsx
@@ -1,6 +1,5 @@
 import { useAppSelector } from '@/hooks/reduxHook';
-import { PHONE_NUMBER_REGEX } from '@/constants/RegExp';
-import { TextInput } from '@/components/UI/TextInput';
+import { PhoneNumber } from '@/components/PhoneNumber';
 import s from './Settings.module.scss';
 
 export const UserPhoneNumber = () => {
@@ -16,16 +15,7 @@ export const UserPhoneNumber = () => {
           {userPhoneNumber ? 'Змінити номер телефону' : 'Додати номер телефону'}
         </p>
       )}
-      <TextInput
-        type='text'
-        id='phoneNumber'
-        placeholder='Номер телефону'
-        value={userPhoneNumber}
-        required={false}
-        regex={PHONE_NUMBER_REGEX}
-        errorMessage='Будь ласка, введіть справний український номер телефону, який розпочинається з +380'
-        editModeIcon
-      />
+      <PhoneNumber userPhoneNumber={userPhoneNumber} />
     </div>
   );
 };
