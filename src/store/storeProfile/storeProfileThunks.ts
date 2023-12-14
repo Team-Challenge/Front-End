161f5726437578b69de594cb9f580477b0995297
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-14 21:38:39 +0200

    feat: add func to get data about store from back

---
+++ src/store/storeProfile/storeProfileThunks.ts
@@ -0,0 +1,49 @@
+import $api from '@/http';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { setName, setDescription, setShopPhoto, setBannerPhoto, setStorePhoneNumber, setLink } from './storeProfileSlice';
+
+export const getStoreInfo = createAsyncThunk(
+  'shops/info',
+  async (_, { dispatch }) => {
+    try {
+      const response = await $api.get('/shops/shop_info');
+      if (response.status === 200) {
+        dispatch(setName(response.data.name));
+        dispatch(setDescription(response.data.description));
+        dispatch(setShopPhoto(response.data.shop_photo));
+        dispatch(setBannerPhoto(response.data.banner_photo));
+        dispatch(setStorePhoneNumber(response.data.phone_number));
+        dispatch(setLink(response.data.link));
+      }
+    } catch (e) {
+      const error = e as Error;
+      throw error;
+    }
+  },
+);
+
+export const changeStoreInfo = createAsyncThunk(
+  'shops/changeInfo',
+  async (
+    credentials: {
+      name?: string | undefined;
+      description?: string | undefined;
+      phone_number?: string | undefined;
+      link?: string | undefined;
+    }) => {
+    try {
+      const response = await $api.post('/shops/shop', {
+        name: credentials.name,
+        description: credentials.description,
+        phone_number: credentials.phone_number,
+        link: credentials.link,
+      });
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (e) {
+      const error = e as Error;
+      throw error;
+    }
+  },
+);
