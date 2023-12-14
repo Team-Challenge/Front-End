c91b4873faf5fd9453181dfbcda14c48eeeade9f
Nick Yefimov <111763794+Nick-Yefimov@users.noreply.github.com>
2023-12-14 19:02:02 +0200

    Fix/delete strict mode (#32)
    * delete strict mode
    
    * added additional verification for tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,11 +6,11 @@ import { App } from './App';
 import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </>,
 );
