757da40506521a60616d83114d38e6be46eb555b
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-14 21:44:50 +0200

    fix: update the list of buttons in the user panel to display the store page correctly

--- src/constants/userPanelButtonsList.tsx
+++ src/constants/userPanelButtonsList.tsx
@@ -1,40 +1,46 @@
+import { useAppSelector } from '@/hooks/reduxHook';
 import { Icon } from '@iconify/react';
 
-export const userPanelButtonsList = [
-  {
-    id: 1,
-    title: 'Особисті дані',
-    pathToPage: 'profile',
-    icon: <Icon icon='solar:user-circle-outline' />,
-  },
-  {
-    id: 2,
-    title: 'Мої покупки',
-    pathToPage: 'purchases',
-    icon: <Icon icon='solar:bag-5-outline' />,
-  },
-  {
-    id: 3,
-    title: 'Обрані товари',
-    pathToPage: 'wishlist',
-    icon: <Icon icon='solar:heart-outline' />,
-  },
-  {
-    id: 4,
-    title: 'Повідомлення',
-    pathToPage: 'messages',
-    icon: <Icon icon='solar:letter-linear' />,
-  },
-  {
-    id: 5,
-    title: 'Мій магазин',
-    pathToPage: 'store',
-    icon: <Icon icon='solar:shop-2-outline' />,
-  },
-  {
-    id: 6,
-    title: 'Налаштування',
-    pathToPage: 'settings',
-    icon: <Icon icon='solar:settings-outline' />,
-  },
-];
+export const getUserPanelButtonsList = () => {
+  const hasStore = useAppSelector((state) => state.storeProfile.name);
+  const storePath = hasStore ? 'store' : 'create-store';
+
+  return [
+    {
+      id: 1,
+      title: 'Особисті дані',
+      pathToPage: 'profile',
+      icon: <Icon icon='solar:user-circle-outline' />,
+    },
+    {
+      id: 2,
+      title: 'Мої покупки',
+      pathToPage: 'purchases',
+      icon: <Icon icon='solar:bag-5-outline' />,
+    },
+    {
+      id: 3,
+      title: 'Обрані товари',
+      pathToPage: 'wishlist',
+      icon: <Icon icon='solar:heart-outline' />,
+    },
+    {
+      id: 4,
+      title: 'Повідомлення',
+      pathToPage: 'messages',
+      icon: <Icon icon='solar:letter-linear' />,
+    },
+    {
+      id: 5,
+      title: 'Мій магазин',
+      pathToPage: storePath,
+      icon: <Icon icon='solar:shop-2-outline' />,
+    },
+    {
+      id: 6,
+      title: 'Налаштування',
+      pathToPage: 'settings',
+      icon: <Icon icon='solar:settings-outline' />,
+    },
+  ];
+};
