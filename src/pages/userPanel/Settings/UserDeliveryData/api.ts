cbf0b8ce36a63d85949ed56553958ddb77581f46
Andrii Moroziuk/PC <morozuk44@gmail.com>
2023-12-15 15:20:17 +0200

    added delivery modal logic and changed api query

--- src/pages/userPanel/Settings/UserDeliveryData/api.ts
+++ src/pages/userPanel/Settings/UserDeliveryData/api.ts
@@ -1,5 +1,9 @@
 import $api from "@/http"
 
-export const getDeliveryData = async () => {
+export const getNovaPostData = async () => {
   return await $api.get(`/orders/nova_post`)
+}
+
+export const getUkrPostData = async () => {
+  return await $api.get(`/orders/urk_post`)
 }
--- src/pages/userPanel/Settings/UserDeliveryData/index.tsx
+++ src/pages/userPanel/Settings/UserDeliveryData/index.tsx
@@ -1,17 +1,32 @@
-import s from '../Settings.module.scss'
+import s from '../Settings.module.scss';
 import { Icon } from '@iconify/react';
 import { Modal } from '@/components/Modal';
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHook';
 import { openModal } from '@/store/modalSlice';
 import { DeliverySelect } from './DeliverySelect';
+import { OrnamentalTitle } from '@/components/OrnamentalTitle';
+import { FormProvider, useForm } from 'react-hook-form';
+import { DeliveryFormData } from '@/types';
+import { UserPhoneNumber } from '../UserPhoneNumber';
+import { FullName } from '@/components/FullName';
+import { PhoneNumber } from '@/components/PhoneNumber';
 
 export const UserDeliveryData = () => {
-  const isModalOpen = useAppSelector(state => state.modal.deliveryInfo)
-  const dispatch = useAppDispatch()
+  const isModalOpen = useAppSelector((state) => state.modal.deliveryInfo);
+  const dispatch = useAppDispatch();
+  const userPhoneNumber = useAppSelector(
+    (state) => state.userProfile.phone_number,
+  );
+  const fullName = useAppSelector((state) => state.userProfile.full_name);
+
+  const methods = useForm<DeliveryFormData>({
+    mode: 'onChange',
+  });
+  const { watch, reset } = methods;
 
   const hanldeOpenModal = () => {
-    dispatch(openModal('deliveryInfo'))
-  }
+    dispatch(openModal('deliveryInfo'));
+  };
 
   return (
     <div className={s.form_wrap}>
@@ -22,15 +37,29 @@ export const UserDeliveryData = () => {
             На жаль, у вас ще не додано жодної адреси доставки
           </p>
           <span className={s.addButton}>
-            <Icon icon='solar:add-circle-outline'/>
+            <Icon icon='solar:add-circle-outline' />
             <button onClick={hanldeOpenModal}>Додати</button>
           </span>
         </>
       )}
       {isModalOpen && (
-        <Modal modalId='deliveryInfo' className={s.modal} >
-          <>delivery</>
-          <DeliverySelect />
+        <Modal modalId='deliveryInfo' className={s.modal}>
+          <OrnamentalTitle tag='h4' text='Введіть адресу' />
+          <FormProvider {...methods}>
+            <form id='delivery' onSubmit={(e) => e.preventDefault()}>
+              <div className={s.form_wrap}>
+                <p className={s.form_hints}>Ім’я та Прізвище од ержувача</p>
+                <FullName value={fullName as string} />
+                <p className={s.form_hints}>
+                  {userPhoneNumber
+                    ? 'Змінити номер телефону'
+                    : 'Додати номер телефону'}
+                </p>
+                <PhoneNumber userPhoneNumber={userPhoneNumber} />
+                <DeliverySelect />
+              </div>
+            </form>
+          </FormProvider>
         </Modal>
       )}
     </div>
