d24e08de11faefa30218d6750ebb242d7ddd04af
Andrii Moroziuk/PC <morozuk44@gmail.com>
2023-12-15 18:32:22 +0200

    feat: add buttons, change small button styles

--- src/pages/userPanel/Settings/UserDeliveryData/DeliveryInfo.tsx
+++ src/pages/userPanel/Settings/UserDeliveryData/DeliveryInfo.tsx
@@ -2,8 +2,8 @@ import { useAppDispatch, useAppSelector } from '@/hooks/reduxHook';
 import { openModal } from '@/store/modalSlice';
 import { DeliveryInfo as TDelivery } from '@/types';
 import { Icon } from '@iconify/react';
-import settings from '../Settings.module.scss'
-import styles from './DeliveryInfo.module.scss'
+import settings from '../Settings.module.scss';
+import styles from './DeliveryInfo.module.scss';
 
 export const DeliveryInfo = () => {
   const dispatch = useAppDispatch();
@@ -22,19 +22,32 @@ export const DeliveryInfo = () => {
         <p className={settings.form_hints}>
           На жаль, у вас ще не додано жодної адреси доставки
         </p>
-        <span className={settings.addButton}>
+        <span className={settings.smallButton}>
           <Icon icon='solar:add-circle-outline' />
           <button onClick={hanldeOpenModal}>Додати</button>
         </span>
       </>
     );
 
-  if (deliveryInfo) return (
-    <div className={styles.delivery}>
-      <p>{deliveryInfo.full_name}</p>
-      <p>{deliveryInfo.phone_number}</p>
-      <p>{deliveryInfo.city_name}</p>
-      <p>{deliveryInfo.branch_name}</p>
-    </div>
-  )
+  if (deliveryInfo)
+    return (
+      <div className={settings.form_wrap}>
+        <div className={styles.delivery_card}>
+          <p>{deliveryInfo.full_name}</p>
+          <p>{deliveryInfo.phone_number}</p>
+          <p>{deliveryInfo.city_name}</p>
+          <p>{deliveryInfo.branch_name}</p>
+        </div>
+        <div className={styles.delivery_buttons}>
+          <span className={settings.smallButton}>
+            <Icon icon='solar:trash-bin' />
+            <button>Змінити</button>
+          </span>
+          <span className={settings.smallButton}>
+            <Icon icon='solar:trash-bin' />
+            <button>Видалити</button>
+          </span>
+        </div>
+      </div>
+    );
 };
