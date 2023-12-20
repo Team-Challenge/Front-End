140dc4a29ce9784191f275fc8772db4421b57488
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-20 17:11:28 +0200

    feat: add a button to add a product

---
+++ src/components/storePanel/AddProductButton/index.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "@iconify/react";
+import s from './AddProductButton.module.scss';
+
+export const AddProductButton = () => {
+  return (
+    <button className={s.button}>
+      Додати товар
+      <Icon icon='solar:add-circle-outline' />
+    </button>
+  );
+};
