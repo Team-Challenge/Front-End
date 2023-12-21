0279fdddfc69ff93293c698373a33c2de668dc0e
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-21 19:10:21 +0200

    feat: add regex for instagram nickname

--- src/constants/RegExp.ts
+++ src/constants/RegExp.ts
@@ -2,3 +2,4 @@ export const FULL_NAME_REGEX = /^(?!.*--)(?!.*``)(?!.*'')[A-Za-zА-Яа-яІіЇ
 export const EMAIL_REGEX = /^(?!.*--)(?!.*__)(?!.*\.\.)(?!.*-$)(?!.*\.$)(?!.*^\..*$)(?!.*\.$)(?!.*^-)[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/g;
 export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/;
 export const PHONE_NUMBER_REGEX = /^\+380\d{9}$/;
+export const INSTAGRAM_NICKNAME_REGEX = /^(?!.*\.\.)(?!.*\.$)@{0,1}[a-zA-Z0-9.,_]{2,30}$/;
