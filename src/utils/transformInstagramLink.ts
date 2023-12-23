0823b8546a2927d94ee5cc1bd67184ecc0870a3e
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-23 12:17:37 +0200

    fix: fix func to transform instagram link to nickname

--- src/utils/transformInstagramLink.ts
+++ src/utils/transformInstagramLink.ts
@@ -5,6 +5,7 @@ export const transformNicknameToInstagramLink = (nickname: string) => {
 }
 
 export const transformInstagramLinkToNickname = (link: string) => {
-  const username = link.match(/instagram.com\/([a-zA-Z0-9_]+)/);
-  return `@${username}`;
+  const regex = /https:\/\/www\.instagram\.com\/([^\/?]+)\/?/i;
+  const match = link.match(regex);
+  return match && `@${match[1]}`;
 };
