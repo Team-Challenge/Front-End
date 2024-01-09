0ad419041966d63ffd6b2f2c597d54d0edd98b21
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-09 21:32:26 +0200

    feat: add a product list component to the store panel

---
+++ src/components/storePanel/ProductsList/index.tsx
@@ -0,0 +1,78 @@
+import { useAppSelector } from '@/hooks/reduxHook';
+import { useWindowDimensions } from '@/hooks/useWindowDimensions';
+import { ProductItemDesktop } from './ProductItemDesktop';
+import { ProductItemMobile } from './ProductItemMobile';
+import { ChangeProductStatus } from '../ChangeProductStatus';
+import testImg from '@assets/default-user-pic.svg';
+import { Icon } from '@iconify/react';
+import s from './ProductsList.module.scss';
+
+const products = [
+  {
+    photo: '',
+    title: 'First product product product product product product',
+    date: '30.01.2024',
+    code: '452158',
+    price: '655',
+    category: 'на руки',
+    status: 'В наявності',
+  },
+  {
+    photo: testImg,
+    title: 'First product',
+    date: '30.01.2024',
+    code: '452158',
+    price: '655',
+    category: 'на руки',
+    status: 'В наявності',
+  },
+  {
+    photo: testImg,
+    title: 'First product',
+    date: '30.01.2024',
+    code: '452158',
+    price: '655',
+    category: 'на руки',
+    status: 'В наявності',
+  },
+];
+
+export const ProductsList = () => {
+  const { width } = useWindowDimensions();
+  const isModalOpen = useAppSelector(
+    (state) => state.modal.changeProductStatus,
+  );
+
+  return (
+    <>
+      {width >= 767.98 ? (
+        <div className={s.table}>
+          <div className={`${s.headline} ${s.row}`}>
+            <p>Фото</p>
+            <p>Назва</p>
+            <p>Дата</p>
+            <p>Код</p>
+            <p>Ціна</p>
+            <p>Категорія</p>
+            <p className={s.headline_status}>
+              Статус <Icon icon='solar:info-circle-outline' />
+            </p>
+          </div>
+          {products.map((product, index) => (
+            <div className={`${s.cell} ${s.row}`} key={index}>
+              <ProductItemDesktop {...product} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={s.block}>
+          {products.map((product, index) => (
+            <ProductItemMobile {...product} key={index} />
+          ))}
+        </div>
+      )}
+
+      {isModalOpen && <ChangeProductStatus />}
+    </>
+  );
+};
