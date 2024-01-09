1a0c5a42c55a03539b2b066bf7ebb39df9650e67
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-09 21:31:43 +0200

    feat: add a mobile version of the product item component to the store panel

---
+++ src/components/storePanel/ProductsList/ProductItemMobile.tsx
@@ -0,0 +1,64 @@
+import { ProductStoreItemProps } from '@/types';
+import { useAppDispatch } from '@/hooks/reduxHook';
+import { openModal } from '@/store/modalSlice';
+import { Icon } from '@iconify/react';
+import s from './ProductsList.module.scss';
+
+export const ProductItemMobile = ({
+  photo,
+  title,
+  date,
+  code,
+  price,
+  category,
+  status,
+}: ProductStoreItemProps) => {
+  const dispatch = useAppDispatch();
+
+  const openModalWindow = () => {
+    dispatch(openModal('changeProductStatus'));
+  };
+
+  return (
+    <div className={s.item}>
+      <div className={s.header}>
+        <div className={s.header_img}>
+          <img src={photo} alt='product' />
+        </div>
+        <p className={s.header_subtitle}>Товар</p>
+        <h5 className={s.header_title}>{title}</h5>
+        <button className={s.header_edit}>
+          <Icon icon='solar:pen-outline' />
+        </button>
+      </div>
+
+      <div className={s.item_wrap}>
+        <p className={s.item_price}>
+          <span>Ціна</span>
+          {price}
+        </p>
+        <p className={s.item_code}>
+          <span>Код</span>
+          {code}
+        </p>
+        <p className={s.item_category}>
+          <span>Категорія</span>
+          {category}
+        </p>
+        <p className={s.item_date}>
+          <span>Дата додавання</span>
+          {date}
+        </p>
+        <div className={s.item_buttons}>
+          <p>
+            <span>Статус</span>
+            <Icon icon='solar:info-circle-outline' />
+          </p>
+          <button className={s.item_status} onClick={openModalWindow}>
+            {status}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
