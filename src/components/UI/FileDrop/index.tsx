74065ee4e450a00eb058735d0437be8f9dd5d01c
Andrii Moroziuk/PC <morozuk44@gmail.com>
2024-01-09 17:53:03 +0200

    feat: file drag-n-drop component functionality

---
+++ src/components/UI/FileDrop/index.tsx
@@ -0,0 +1,94 @@
+import { FileDropProps } from "@/types";
+import s from "./FileDrop.module.scss"
+import { ChangeEvent, useRef, useState } from "react";
+
+export const FileDrop = ({
+  children,
+  onChange = (items) => { console.log('Changed Items: ', items) },
+  isMulti = false,
+  allowedFormats
+}: FileDropProps) => {
+  const [isDraggerOver, setIsDraggedOver] = useState(false)
+  const [files, setFiles] = useState()
+  const [error, setError] = useState<string | null>(null)
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+
+  const onChangeFiles = (files: FileList) => {
+    setError(null)
+    let localError = null
+
+    if (!isMulti && files.length > 1) {
+      localError = 'Можливо додати лише один файл'
+    }
+
+    Object.values(files).forEach((file) => {
+      if (!isValidType(file)) {
+        localError = 'Невірний тип файлів'
+      }
+    })
+
+    console.log('localError:', !!localError);
+
+    !localError ? onChange(files) : setError(localError)
+    localError = null
+  }
+
+  const isValidType = (file: any) => {
+    return !allowedFormats?.length || allowedFormats?.some(format => file.name.toLowerCase().endsWith(`${format}`)) ||
+      allowedFormats?.includes(file.type)
+  }
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDraggedOver(true)
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  }
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDraggedOver(false)
+  }
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  }
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDraggedOver(false)
+    onChangeFiles(e.dataTransfer.files)
+  }
+
+  const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    e.target.files && onChangeFiles(e.target.files)
+  }
+
+  return (
+    <div
+      className={
+        `${s.file_drop}
+        ${isDraggerOver ? s.file_drop_is_dragged : null}`
+      }
+      onClick={handleClick}
+      onDragEnter={handleDragEnter}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
+      <div className={s.file_input_content}>
+        {children || 'Натисніть щоб завантажити файли'}
+        <div>{error}</div>
+      </div>
+      <input
+        ref={fileInputRef}
+        type="file"
+        onChange={handleFileInput}
+        className={s.file_drop_file_input}
+      />
+    </div >
+  );
+}
