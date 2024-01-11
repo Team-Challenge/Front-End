22e0bdc5669a1c66e987ba872536dd8666ff38ba
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-11 17:13:01 +0200

    feat: add markup for the "Reviews and Rating" screen in the store panel

--- src/pages/storePanel/Reviews/index.tsx
+++ src/pages/storePanel/Reviews/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import { ReviewStatisticsPanel } from '@/components/storePanel/ReviewStatisticsPanel';
+import { ReviewsList } from '@/components/storePanel/ReviewsList';
 import { EmptyContentPage } from '@/components/EmptyContentPage';
 import { Icon } from '@iconify/react';
 import s from './Reviews.module.scss';
 
 export const Reviews = () => {
+  const [isReviewsAvailable, setIsReviewsAvailable] = useState<boolean>(true);
+
   return (
-    <section>
-      <EmptyContentPage
-        title='Ой, тут поки пусто'
-        text='Здається, ще ніхто не залишив відгуки про ваші товари. Коли хтось придбає та оцінить ваш товар, нові відгуки та рейтинг з’являться тут'
-        item={<Icon icon='solar:chat-round-like-outline' />}
-      />
-      {/* <h4>Відгуки та рейтинг</h4> */}
-    </section>
+    <>
+      {isReviewsAvailable ? (
+        <section className={s.reviews}>
+          <h4 className={s.reviews_title}>Відгуки та рейтинг</h4>
+          <ReviewStatisticsPanel />
+          <ReviewsList />
+        </section>
+      ) : (
+        <EmptyContentPage
+          title='Ой, тут поки пусто'
+          text='Здається, ще ніхто не залишив відгуки про ваші товари. Коли хтось придбає та оцінить ваш товар, нові відгуки та рейтинг з’являться тут'
+          item={<Icon icon='solar:chat-round-like-outline' />}
+        />
+      )}
+    </>
   );
 };
