691d98ad54c05f0b7fa80364c2fea0e6314af41e
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-16 18:15:15 +0200

    feat: add an Order to the store panel page

--- src/pages/storePanel/Orders/index.tsx
+++ src/pages/storePanel/Orders/index.tsx
@@ -1,16 +1,45 @@
+import { useState } from 'react';
+import { purchaseStatusList } from '@/constants/purchaseStatusList';
+import { OrdersList } from '@/components/storePanel/orders/OrdersList';
 import { EmptyContentPage } from '@/components/EmptyContentPage';
 import { Icon } from '@iconify/react';
 import s from './Orders.module.scss';
 
 export const Orders = () => {
+  const [isOrdersAvailable, setIsOrdersAvailable] = useState<boolean>(true);
+  const [activeButtonId, setActiveButtonId] = useState<number>(1);
+
+  const handleButtonClick = (buttonId: number) => {
+    setActiveButtonId(buttonId);
+  };
+
   return (
-    <section>
-      <EmptyContentPage
-        title='Ой, тут поки пусто'
-        text='Здається, ще ніхто не встиг придбати ваші товари. Коли це станеться, історія та статуси ваших продажів з’являться тут.'
-        item={<Icon icon='solar:tag-outline' />}
-      />
-      {/* <h4>Замовлення</h4> */}
-    </section>
+    <>
+      {isOrdersAvailable ? (
+        <section className={s.orders}>
+          <h4 className={s.orders_title}>Замовлення</h4>
+          <div className={s.orders_status}>
+            {purchaseStatusList.map(({ id, label }) => (
+              <button
+                key={id}
+                className={`${s.orders_status_button} ${
+                  id === activeButtonId ? s.active : ''
+                }`}
+                onClick={() => handleButtonClick(id)}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
+          <OrdersList />
+        </section>
+      ) : (
+        <EmptyContentPage
+          title='Ой, тут поки пусто'
+          text='Здається, ще ніхто не встиг придбати ваші товари. Коли це станеться, історія та статуси ваших продажів з’являться тут.'
+          item={<Icon icon='solar:tag-outline' />}
+        />
+      )}
+    </>
   );
 };
