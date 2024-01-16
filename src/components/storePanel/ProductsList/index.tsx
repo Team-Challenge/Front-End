bed810f30c1593cfeaf8d3b0780701e25016dc60
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-16 18:33:23 +0200

    fix: fix style of the product list in the store panel

--- src/components/storePanel/ProductsList/index.tsx
+++ src/components/storePanel/ProductsList/index.tsx
@@ -4,22 +4,12 @@ import { Tooltip } from '@/components/UI/Tooltip';
 import { ProductItemDesktop } from './ProductItemDesktop';
 import { ProductItemMobile } from './ProductItemMobile';
 import { ChangeProductStatus } from '../ChangeProductStatus';
-import testImg from '@assets/default-user-pic.svg';
 import { Icon } from '@iconify/react';
 import s from './ProductsList.module.scss';
 
 const products = [
   {
     photo: '',
-    title: 'First product product product product product product',
-    date: '30.01.2024',
-    code: '452158',
-    price: '655',
-    category: 'на руки',
-    status: 'В наявності',
-  },
-  {
-    photo: testImg,
     title: 'First product',
     date: '30.01.2024',
     code: '452158',
@@ -28,8 +18,17 @@ const products = [
     status: 'В наявності',
   },
   {
-    photo: testImg,
-    title: 'First product',
+    photo: '',
+    title: 'Second product',
+    date: '30.01.2024',
+    code: '452158',
+    price: '655',
+    category: 'на руки',
+    status: 'В наявності',
+  },
+  {
+    photo: '',
+    title: 'Third product',
     date: '30.01.2024',
     code: '452158',
     price: '655',
@@ -46,7 +45,7 @@ export const ProductsList = () => {
 
   return (
     <>
-      {width >= 767.98 ? (
+      {width >= 1260 ? (
         <div className={s.table}>
           <div className={`${s.headline} ${s.row}`}>
             <p>Фото</p>
@@ -55,15 +54,15 @@ export const ProductsList = () => {
             <p>Код</p>
             <p>Ціна</p>
             <p>Категорія</p>
-            <p className={s.headline_status}>
-              Статус
+            <div className={s.headline_status}>
+              <p>Статус</p>
               <Tooltip
                 text='Щоб змінити статус товару, просто клікніть на поточний статус та оберіть інший'
                 className={s.headline_tooltip}
               >
                 <Icon icon='solar:info-circle-outline' />
               </Tooltip>
-            </p>
+            </div>
           </div>
           {products.map((product, index) => (
             <div className={`${s.cell} ${s.row}`} key={index}>
