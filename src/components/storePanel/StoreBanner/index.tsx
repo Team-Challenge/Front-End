8a94b856751e829108c1b584236a29d126bb65f8
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-19 16:47:10 +0200

    refactor: refactor StoreBanner after using prettier rules

--- src/components/storePanel/StoreBanner/index.tsx
+++ src/components/storePanel/StoreBanner/index.tsx
@@ -1,25 +1,33 @@
-import { FileDrop } from "@/components/UI/FileDrop";
-import { Icon } from "@iconify/react";
-import s from './StoreBanner.module.scss'
-import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook";
-import { changeBanner, deleteBanner, getStoreInfo } from "@/store/storeProfile/storeProfileThunks";
-import { useRef } from "react";
-
+import { useRef } from 'react';
+import { useAppDispatch, useAppSelector } from '@/hooks/reduxHook';
+import {
+  changeBanner,
+  deleteBanner,
+  getStoreInfo,
+} from '@/store/storeProfile/storeProfileThunks';
+import { FileDrop } from '@/components/UI/FileDrop';
+import { Icon } from '@iconify/react';
+import s from './StoreBanner.module.scss';
 
 export const StoreBanner = () => {
-  const dispatch = useAppDispatch()
-  const { banner_photo } = useAppSelector(state => state.storeProfile)
-  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const dispatch = useAppDispatch();
+  const { banner_photo } = useAppSelector((state) => state.storeProfile);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const handleBannerUpload = async (files: FileList) => {
-    const file = files[0]
+    const file = files[0];
     if (!file) return;
 
-    const allowedFormats = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp']
+    const allowedFormats = [
+      'image/jpeg',
+      'image/jpg',
+      'image/png',
+      'image/webp',
+    ];
     const fileSize = file.size / 1024 / 1024;
 
     if (!allowedFormats?.includes(file.type || '')) {
-      alert('Дозволені формати файлів: png, jpg, jpeg, webp')
+      alert('Дозволені формати файлів: png, jpg, jpeg, webp');
       return;
     }
 
@@ -28,59 +36,58 @@ export const StoreBanner = () => {
       return;
     }
 
-    const formData = new FormData()
+    const formData = new FormData();
     formData.append('image', file);
-    await dispatch(changeBanner(formData))
-    dispatch(getStoreInfo())
-  }
+    await dispatch(changeBanner(formData));
+    dispatch(getStoreInfo());
+  };
 
   const handleDeleteBanner = async () => {
-    await dispatch(deleteBanner())
-    dispatch(getStoreInfo())
-  }
+    await dispatch(deleteBanner());
+    dispatch(getStoreInfo());
+  };
 
   const handleUploadClick = () => {
-    fileInputRef.current?.click()
-  }
+    fileInputRef.current?.click();
+  };
 
   const handleBannerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    e.target.files && handleBannerUpload(e.target.files)
-  }
+    e.preventDefault();
+    e.target.files && handleBannerUpload(e.target.files);
+  };
 
   return (
     <>
       <div className={s.banner}>
         <div className={s.banner_image}>
-          {banner_photo ?
-            <img src={banner_photo} alt='banner' /> :
-            <FileDrop
-              onChange={handleBannerUpload}
-            >
+          {banner_photo ? (
+            <img src={banner_photo} alt='banner' />
+          ) : (
+            <FileDrop onChange={handleBannerUpload}>
               <div className={s.banner_inner_text}>
                 <p>Завантажити фото банера</p>
-                <Icon icon="solar:camera-outline" />
+                <Icon icon='solar:camera-outline' />
               </div>
             </FileDrop>
-          }
+          )}
         </div>
 
         <div className={s.banner_buttons}>
           <div>
-            <Icon icon="solar:camera-outline" />
+            <Icon icon='solar:camera-outline' />
             <button onClick={handleUploadClick}>Завантажити нове фото</button>
             <input
-              type="file"
+              type='file'
               ref={fileInputRef}
               onChange={handleBannerInput}
             />
           </div>
           <div>
-            <Icon icon="solar:trash-bin-trash-outline" />
+            <Icon icon='solar:trash-bin-trash-outline' />
             <button onClick={handleDeleteBanner}>Видалити</button>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
