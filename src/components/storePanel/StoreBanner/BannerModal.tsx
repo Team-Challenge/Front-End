194264d5f6eb8841e0aec2dbdd7ed1e8d146dd83
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-21 18:45:32 +0200

    refactor: change the name of style classes and sort imports

--- src/components/storePanel/StoreBanner/BannerModal.tsx
+++ src/components/storePanel/StoreBanner/BannerModal.tsx
@@ -1,61 +1,54 @@
-import { Modal } from "@/components/Modal";
-import { OrnamentalTitle } from "@/components/OrnamentalTitle";
-import { FileDrop } from "@/components/UI/FileDrop";
-import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook";
+import { useState } from 'react';
+import { BannerModalProps } from '@/types';
+import { useAppDispatch, useAppSelector } from '@/hooks/reduxHook';
+import { closeModal } from '@/store/modalSlice';
+import { Modal } from '@/components/Modal';
+import { OrnamentalTitle } from '@/components/OrnamentalTitle';
+import { ButtonUI } from '@/components/UI';
+import { FileDrop } from '@/components/UI/FileDrop';
+import { Icon } from '@iconify/react';
 import s from './StoreBanner.module.scss';
-import { Icon } from "@iconify/react";
-import { ButtonUI } from "@/components/UI";
-import { useState } from "react";
-import { closeModal } from "@/store/modalSlice";
-
-interface BannerModalProps {
-  handleBannerUpload: (files: FileList) => Promise<void>
-}
 
 export const BannerModal = ({ handleBannerUpload }: BannerModalProps) => {
-  const [files, setFiles] = useState<FileList | null>(null)
-  const dispatch = useAppDispatch()
-  const isModalOpen = useAppSelector((state) =>
-    state.modal.storeBanner
-  );
+  const [files, setFiles] = useState<FileList | null>(null);
+  const dispatch = useAppDispatch();
+  const isModalOpen = useAppSelector((state) => state.modal.storeBanner);
 
   const handleSave = () => {
-    files && handleBannerUpload(files)
-    setFiles(null)
-    dispatch(closeModal('storeBanner'))
-  }
+    files && handleBannerUpload(files);
+    setFiles(null);
+    dispatch(closeModal('storeBanner'));
+  };
 
   const handleAddImage = (files: FileList) => {
     setFiles(files);
-  }
+  };
 
   return (
     <>
-      {isModalOpen &&
+      {isModalOpen && (
         <Modal modalId={'storeBanner'} className={s.modal}>
-          <OrnamentalTitle
-            tag='h4'
-            text={'Фото банера'}
-            className={s.modal_title}
-          />
-          <FileDrop onChange={handleAddImage} className={s.modal_file_drop}>
-            {files ? <>
-              <img src={URL.createObjectURL(files[0])} alt='banner' className={s.modal_image} />
-            </> : <>
-              <div className={s.modal_inner_text}>
+          <OrnamentalTitle tag='h4' text='Фото банера' />
+          <FileDrop onChange={handleAddImage} className={s.modal_block}>
+            {files ? (
+              <div className={s.modal_image}>
+                <img src={URL.createObjectURL(files[0])} alt='banner' />
+              </div>
+            ) : (
+              <div className={s.modal_text}>
                 <p>Натисніть, щоб завантажити фото</p>
                 <Icon icon='solar:camera-outline' />
               </div>
-            </>}
+            )}
           </FileDrop>
-          <p>Допустимі розміри банера 360х160</p>
+          <p className={s.modal_hint}>Допустимі розміри банера 360х160</p>
           <ButtonUI
             label='Зберегти'
             onClick={handleSave}
             className={s.modal_button}
           />
         </Modal>
-      }
+      )}
     </>
-  )
-}
+  );
+};
