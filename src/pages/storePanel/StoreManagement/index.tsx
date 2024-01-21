48db570781b5f78fab09f6453a7c06b656116932
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-21 19:51:28 +0200

    feat: add style to profile photo to display according to the design

--- src/pages/storePanel/StoreManagement/index.tsx
+++ src/pages/storePanel/StoreManagement/index.tsx
@@ -1,9 +1,9 @@
 import { StoreSettings } from '@/components/storePanel/StoreSettings';
 import { useAppSelector } from '@/hooks/reduxHook';
+import { StoreBanner } from '@/components/storePanel/StoreBanner';
 import { ProfilePhoto } from '@/components/ProfilePhoto';
 import defaultStorePic from '@assets/default-store-pic.svg';
 import s from './StoreManagement.module.scss';
-import { StoreBanner } from '@/components/storePanel/StoreBanner';
 
 export const StoreManagement = () => {
   const storePhoto = useAppSelector((state) => state.storeProfile.shop_photo);
@@ -16,6 +16,7 @@ export const StoreManagement = () => {
         defaultPhoto={defaultStorePic}
         modalId='storePhoto'
         profilePhoto={storePhoto}
+        className={s.settings_photo}
       />
       <h4 className={s.settings_title}>Налаштування магазину</h4>
       <StoreSettings />
