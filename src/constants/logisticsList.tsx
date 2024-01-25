0499464d2494dbd9ca69cf11ac06c6d3fcc9c335
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-25 21:43:12 +0200

    feat: add a list of data for the ProductAdd page

---
+++ src/constants/logisticsList.tsx
@@ -0,0 +1,39 @@
+import { Icon } from '@iconify/react';
+import novaPostLogo from '@assets/nova-post-logo.svg';
+import ukrPostLogo from '@assets/ukr-post-logo.svg';
+
+export const paymentMethods = [
+  {
+    id: 'card',
+    icon: <Icon icon='solar:card-outline' />,
+    title: 'Оплата на карту',
+    desc: 'Оплата вартості товару на карту',
+  },
+  {
+    id: 'cash',
+    icon: <Icon icon='solar:wallet-outline' />,
+    title: 'Оплата при отриманні (Накладений платіж)',
+    desc: 'Комісія за тарифами перевізника (Нова Пошта від 40 грн)',
+  },
+  {
+    id: 'secure-payment',
+    icon: <Icon icon='solar:shield-check-outline' />,
+    title: 'Безпечна оплата',
+    desc: 'Ви отримає кошти після того як покупець отримає та перевірить товар',
+  },
+];
+
+export const deliveryMethods = [
+  {
+    id: 'novaPost',
+    icon: novaPostLogo,
+    title: 'Нова пошта',
+    desc: 'від 50 грн',
+  },
+  {
+    id: 'ukrPost',
+    icon: ukrPostLogo,
+    title: 'Укрпошта',
+    desc: 'Акція! 30 грн (до 15 кг), 70 грн (понад 15 кг)',
+  },
+];
