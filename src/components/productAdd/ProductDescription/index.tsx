dc9f64e1034f3978ea634157572ecef281e43901
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-29 17:32:59 +0200

    feat: add components for product name and description

---
+++ src/components/productAdd/ProductDescription/index.tsx
@@ -0,0 +1,29 @@
+import { TextArea, Tooltip } from '@/components/UI';
+import { Icon } from '@iconify/react';
+import s from './ProductDescription.module.scss';
+
+export const ProductDescription = () => {
+  return (
+    <div className={s.description}>
+      <div className='product-add_subtitle_wrap'>
+        <p className='product-add_subtitle'>Опис товару</p>
+        <Tooltip
+          text='Не знаєте що написати?
+          Почніть з опису вигляду самого виробу, з яких матеріалів виготовлений, 
+          опишіть процес створення, щоб підкреслити його характер та індивідуальність'
+          className='product-add_tooltip'
+        >
+          <Icon icon='heroicons:light-bulb' />
+        </Tooltip>
+      </div>
+      <TextArea
+        name='description'
+        id='description'
+        placeholder='Приклад: Реконструкція старовинної карпатської силянки, виготовлена з червоного чеського бісеру...'
+        rows={11}
+        maxLength={1000}
+        minLength={1}
+      />
+    </div>
+  );
+};
---
+++ src/components/productAdd/ProductName/index.tsx
@@ -0,0 +1,53 @@
+import { useFormContext } from 'react-hook-form';
+import { useCharCount } from '@/hooks/useCharCount';
+import { TextInput, Tooltip } from '@/components/UI';
+import { Icon } from '@iconify/react';
+import s from './ProductName.module.scss';
+
+export const ProductName = () => {
+  const methods = useFormContext();
+  const {
+    formState: { errors },
+  } = methods;
+  const { charCount, handleInput } = useCharCount('', 30);
+
+  const hasError = Boolean(errors.name);
+
+  return (
+    <div className={`${s.form_wrap} ${s.name}`}>
+      <div className='product-add_subtitle_wrap'>
+        <p className='product-add_subtitle'>
+          Назва товару<span>*</span>
+        </p>
+        <Tooltip
+          text='Чітка та точна назва без помилок – значно полегшить пошук для покупців'
+          className='product-add_tooltip'
+        >
+          <Icon icon='heroicons:light-bulb' />
+        </Tooltip>
+      </div>
+      <TextInput
+        type='text'
+        id='name'
+        placeholder='Наприклад: Сережки з дерева “Ластівки”'
+        // value={productName}
+        required
+        errorMessage={`Будь ласка, введіть назву свого товару`}
+        maxLength={100}
+        onClick={() => methods.clearErrors('name')}
+        onInput={(event) => handleInput(event)}
+        editModeIcon
+      />
+
+      {hasError ? (
+        <p className={`error-text ${s.form_error}`}>
+          {errors?.name?.message as string}
+        </p>
+      ) : (
+        <p className={s.name_char_count}>
+          {charCount}/{100} символів
+        </p>
+      )}
+    </div>
+  );
+};
