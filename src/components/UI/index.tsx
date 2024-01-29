9371d75a891da9246b7f83921ed3d6ea5f457fb0
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-01-29 22:03:26 +0200

    feat: add new UI components

--- src/components/UI/index.tsx
+++ src/components/UI/index.tsx
@@ -2,5 +2,20 @@ import { ButtonUI } from './ButtonUI';
 import { TextInput } from './TextInput';
 import { TextArea } from './TextArea';
 import { PasswordInput } from './PasswordInput';
+import { FileDrop } from './FileDrop';
+import { ToggleSwitch } from './ToggleSwitch';
+import { Tooltip } from './Tooltip';
+import { QuantityInput } from './QuantityInput';
+import { SelectInput } from './SelectInput';
 
-export { ButtonUI, TextInput, TextArea, PasswordInput };
+export {
+  ButtonUI,
+  TextInput,
+  TextArea,
+  PasswordInput,
+  FileDrop,
+  ToggleSwitch,
+  Tooltip,
+  QuantityInput,
+  SelectInput
+};
