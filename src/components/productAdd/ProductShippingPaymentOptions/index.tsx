5e4d3c7526449be133b34755b9cca7148c8eb136
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-02-02 21:51:49 +0200

    fix: add error handling and fix styles in payment and delivery component

--- src/components/productAdd/ProductShippingPaymentOptions/index.tsx
+++ src/components/productAdd/ProductShippingPaymentOptions/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 import {
   deliveryMethods,
   paymentMethods,
@@ -8,13 +8,19 @@ import { ToggleSwitch } from '@/components/UI';
 import s from './ProductShippingPaymentOptions.module.scss';
 
 export const ProductShippingPaymentOptions = () => {
-  const { setValue } = useFormContext();
   const [selectedPaymentMethods, setSelectedPaymentMethods] = useState<
     string[]
   >([]);
   const [selectedDeliveryMethods, setSelectedDeliveryMethods] = useState<
     string[]
   >([]);
+
+  const {
+    control,
+    setValue,
+    clearErrors,
+    formState: { errors },
+  } = useFormContext();
 
   const handleSelectedPaymentMethod = (id: string) => {
     if (selectedPaymentMethods.includes(id)) {
@@ -44,45 +50,88 @@ export const ProductShippingPaymentOptions = () => {
 
   return (
     <>
-      <div>
+      <div className={s.payment}>
         <p className='product-add_subtitle'>
           Оберіть способи оплати<span>*</span>
         </p>
-        <ul className={s.logistics_list}>
+        <ul className={s.list}>
           {paymentMethods.map(({ id, icon, title, desc }) => (
-            <li key={id} className={s.logistics_item}>
-              <i className={s.logistics_icon}>{icon}</i>
-              <h6 className={s.logistics_title}>{title}</h6>
-              <p className={s.logistics_desc}>{desc}</p>
-              <ToggleSwitch
-                name='payment'
-                id={id}
-                className={s.logistics_toggle}
-                onChange={() => handleSelectedPaymentMethod(id)}
+            <li key={id} className={s.item}>
+              <i className={s.item_icon}>{icon}</i>
+              <h6 className={s.item_title}>{title}</h6>
+              <p className={s.item_desc}>{desc}</p>
+              <Controller
+                name='paymentMethods'
+                control={control}
+                rules={{
+                  required:
+                    selectedPaymentMethods.length > 0
+                      ? false
+                      : 'Будь ласка, оберіть принаймні один варіант оплати.',
+                }}
+                render={({ field }) => (
+                  <ToggleSwitch
+                    field={field}
+                    id={id}
+                    className={s.item_toggle}
+                    onChange={() => {
+                      handleSelectedPaymentMethod(id);
+                      clearErrors('paymentMethods');
+                    }}
+                  />
+                )}
               />
             </li>
           ))}
         </ul>
+
+        {errors.paymentMethods && (
+          <p className='error-text'>
+            {errors.paymentMethods.message as string}
+          </p>
+        )}
       </div>
 
-      <div>
+      <div className={s.delivery}>
         <p className='product-add_subtitle'>
           Оберіть способи доставки<span>*</span>
         </p>
-        <ul className={s.logistics_list}>
+        <ul className={s.list}>
           {deliveryMethods.map(({ id, icon, title, desc }) => (
-            <li key={id} className={s.logistics_item}>
-              <img src={icon} alt={title} className={s.logistics_icon} />
-              <h6 className={s.logistics_title}>{title}</h6>
-              <p className={s.logistics_desc}>{desc}</p>
-              <ToggleSwitch
-                id={id}
-                className={s.logistics_toggle}
-                onChange={() => handleSelectedDeliveryMethod(id)}
+            <li key={id} className={s.item}>
+              <img src={icon} alt={title} className={s.item_icon} />
+              <h6 className={s.item_title}>{title}</h6>
+              <p className={s.item_desc}>{desc}</p>
+              <Controller
+                name='deliveryMethods'
+                control={control}
+                rules={{
+                  required:
+                    selectedDeliveryMethods.length > 0
+                      ? false
+                      : 'Будь ласка, оберіть принаймні один варіант доставки.',
+                }}
+                render={({ field }) => (
+                  <ToggleSwitch
+                    field={field}
+                    id={id}
+                    className={s.item_toggle}
+                    onChange={() => {
+                      handleSelectedDeliveryMethod(id);
+                      clearErrors('deliveryMethods');
+                    }}
+                  />
+                )}
               />
             </li>
           ))}
         </ul>
+
+        {errors.deliveryMethods && (
+          <p className='error-text'>
+            {errors.deliveryMethods.message as string}
+          </p>
+        )}
       </div>
     </>
   );
