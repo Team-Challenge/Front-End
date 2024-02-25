9cd7b47476bb403184ce89b99e3d8d50accf916c
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-02-25 22:34:43 +0200

    feat: add sorting options

---
+++ src/constants/sortingOptions.ts
@@ -0,0 +1,6 @@
+export const sortingOptions = [
+  { id: 'popular', label: 'за популярністю' },
+  { id: 'date', label: 'за датою додавання' },
+  { id: 'priceLowToHigh', label: 'від дешевих до дорогих' },
+  { id: 'priceHighToLow', label: 'від дорогих до дешевих' },
+];
