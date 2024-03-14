aeb5816d9d299b75270e5a712b3349d9a7840683
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-03-14 19:54:58 +0200

    refactor: move files to another folder for a clearer project structure

--- src/pages/storePanel/Orders/index.tsx
+++ src/pages/storePanel/Orders/index.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import { purchaseStatusList, emptySectionText } from '@/constants';
 import { ButtonsBar } from '@/components/ButtonsBar';
-import { OrdersList } from '@/components/storePanel/orders/OrdersList';
+import { OrdersList } from '@/pages/storePanel/components/orders/OrdersList';
 import { EmptyContentPage } from '@/components/EmptyContentPage';
 import { Icon } from '@iconify/react';
+import { useWindowDimensions } from '@/hooks/useWindowDimensions';
 import s from './Orders.module.scss';
 
 export const Orders = () => {
+  const { width } = useWindowDimensions();
   const [isOrdersAvailable, setIsOrdersAvailable] = useState<boolean>(true);
 
   return isOrdersAvailable ? (
     <section className={s.orders}>
-      <h4 className={s.orders_title}>Замовлення</h4>
+      {width >= 991.98 && <h4 className={s.orders_title}>Замовлення</h4>}
       <ButtonsBar buttonsList={purchaseStatusList} />
       <OrdersList />
     </section>
